53a41f6b16478eb8abc8e403f177b541d155143b
Nicolas Paquier-Brea <37303758+Npaquier-brea@users.noreply.github.com>
2022-06-03 19:01:21 +0200

    mintNFT

--- components/ButtonConnexionWallet/ConnexionWallet.js
+++ components/ButtonConnexionWallet/ConnexionWallet.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ConnectedButton from "./ConnectedButton";
-import { TezosToolkit } from "@taquito/taquito";
+import { TezosToolkit, MichelsonMap } from "@taquito/taquito";
+import { char2Bytes } from '@taquito/tzip16';
 import { BeaconWallet } from "@taquito/beacon-wallet";
 import { NetworkType } from "@airgap/beacon-sdk";
+import * as config from '../../config/config.js'
 
 export default function ConnexionWallet() {
-  const network = { type: NetworkType.MAINNET };
-  const Tezos = new TezosToolkit("https://mainnet-tezos.giganode.io"); // TODO: url env variable
+  const network = { type: NetworkType.ITHACANET };
+  const Tezos = new TezosToolkit(config.RPC_URL); // TODO: url env variable
 
   const wallet = new BeaconWallet({
-    name: "Beacon Docs",
-    preferredNetwork: network.type,
+    name: config.NAME,
+    preferredNetwork: config.NETWORK,
   });
 
   Tezos.setWalletProvider(wallet);
@@ -19,7 +21,6 @@ export default function ConnexionWallet() {
 
   /*** Function to connect to the wallet ***/
   const connectToWallet = async () => {
-    console.log("je penerte êtasse")
     const activeAccount = await wallet.client.getActiveAccount();
     if (activeAccount) {
       setMyAddress(activeAccount.address);
@@ -32,9 +33,34 @@ export default function ConnexionWallet() {
     }
   };
 
+  /*** Function to get the smart contract ***/
+  const getSmartContract = async () => {
+    const contract = await Tezos.wallet.at(config.CONTRACT_ADDRESS);
+    return contract;
+  }
+
+  /*** Function to mint the nft ***/
+  const mintNFT = async (address, url, token_id) => {
+    await disconnect();
+    await connectToWallet();
+    const amount = 1;
+    const contract = await getSmartContract();
+    url = char2Bytes(url);
+    const op = await contract.methods.mint(address, amount, MichelsonMap.fromLiteral({ '': url }), token_id).send({ amount: 1000 });
+    return await op.confirmation(3);
+  };
+
+  const getTezos = async () => {
+    await disconnect();
+    await connectToWallet();
+    const amount = 1;
+    const contract = await getSmartContract();
+    const op = await contract.methods.transfer_mutez(1000, "tz1fm7T5D5EyEa5aJWMzAvSwZcwAdwUfDx5C").send()
+    return await op.confirmation(3);
+  }
+
   /*** Function to connect to the wallet ***/
   const disconnect = async () => {
-    console.log("me dis pas que ?")
     await new Promise((resolve) => setTimeout(resolve, 1000));
     await wallet.clearActiveAccount();
     await wallet.disconnect();
@@ -44,6 +70,20 @@ export default function ConnexionWallet() {
   /*** Render ***/
   return (
     <div>
+      <button
+        onClick={() => mintNFT("tz1QQTyMYNP33e8bgkmHgnP7DUUgsyi6G63k", "ipfs://bafkreihf6wzky7hxrky4efde3tdxxu6xdarwyoagddvsraoxfmskz2pzr4", 2)}
+        className="bg-white text-lightBlue-600  active:bg-blueGray-600 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3  ease-linear transition-all duration-150"
+        type="button"
+      >
+        mintNFT
+      </button>
+      <button
+        onClick={() => getTezos()}
+        className="bg-white text-lightBlue-600  active:bg-blueGray-600 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3  ease-linear transition-all duration-150"
+        type="button"
+      >
+        getTezos
+      </button>
       <button
         onClick={() => connectToWallet()}
         className="bg-white text-lightBlue-600  active:bg-blueGray-600 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3  ease-linear transition-all duration-150"
---
+++ config/config.js
@@ -0,0 +1,4 @@
+export const NAME = 'NFPets'; // the name of the app
+export const CONTRACT_ADDRESS = 'KT1WKX7tm6ZWmCvfw3j81Bb98fCGuEiZRSrG';
+export const RPC_URL = 'https://rpc.ithacanet.teztnets.xyz/'; // network rpc url
+export const NETWORK = 'ithacanet';
