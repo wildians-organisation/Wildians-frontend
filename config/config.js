5c0a3b210ebfb8c0fda0a180f1099759822a70ec
Nicolas Paquier-Brea <37303758+Npaquier-brea@users.noreply.github.com>
2022-06-07 11:54:44 +0200

    mintNFT fixed

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 export const NAME = 'NFPets'; // the name of the app
 export const CONTRACT_ADDRESS = 'KT1WKX7tm6ZWmCvfw3j81Bb98fCGuEiZRSrG';
-export const RPC_URL = 'https://rpc.ithacanet.teztnets.xyz/'; // network rpc url
+export const RPC_URL = 'https://ithacanet.smartpy.io'; // network rpc url
 export const NETWORK = 'ithacanet';
