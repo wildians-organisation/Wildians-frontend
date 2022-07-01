8242436f29a717abed3d67619f3e9025b0a5feb7
Nicolas Paquier-Brea <37303758+Npaquier-brea@users.noreply.github.com>
2022-07-01 10:09:00 +0200

    last push

--- pages/Transactions/index.jsx
+++ pages/Transactions/index.jsx
@@ -7,12 +7,18 @@ export default function Transactions() {
   return (
     <>
       <main className="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75">
-        <div style={{ marginTop: 290 }}>
-          <center>
-            <p>Welcome to the best Transactions page of the universe!</p>
-            <p>Work in progress...</p>
-          </center>
-        </div>
+        <form action="/send-data-here" method="post">
+          <label for="address">Recipient:</label>
+          <input type="text" id="address" name="address" />
+          <button
+            onClick={() => sendData()}
+            className="bg-white text-lightBlue-600  active:bg-blueGray-600 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3  ease-linear transition-all duration-150"
+            type="button"
+          >
+            send Data
+          </button>
+        </form>
       </main>
+
     </>);
 };
