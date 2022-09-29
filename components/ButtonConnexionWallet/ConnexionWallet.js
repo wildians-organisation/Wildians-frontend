3a4f52889410a88a9d67a9eb159184ff35bcf16f
nicolas.paquier-brea <nicolas.paquier-brea@epita.fr>
2022-09-29 23:49:40 +0200

    add mint variable  in config and increment token

--- components/ButtonConnexionWallet/ConnexionWallet.js
+++ components/ButtonConnexionWallet/ConnexionWallet.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { useState, createContext, useContext } from "react";
+import { useContext } from "react";
 import ConnectedButton from "./ConnectedButton";
 import { TezosToolkit, MichelsonMap } from "@taquito/taquito";
 import { char2Bytes } from '@taquito/tzip16';
 import { BeaconWallet } from "@taquito/beacon-wallet";
 import { NetworkType } from "@airgap/beacon-sdk";
 import * as config from '../../config/config.js'
-import { address } from "pages/transactions";
 
+const token_id = 0;
 const network = { type: NetworkType.JAKARTANET };
 const Tezos = new TezosToolkit(config.RPC_URL); // TODO: url env variable
 const walletContext = React.createContext(
@@ -15,7 +15,7 @@ const walletContext = React.createContext(
     name: config.NAME,
     preferredNetwork: config.NETWORK,
   })
-);
+)
 
 export default function ConnexionWallet() {
 
@@ -46,15 +46,13 @@ export default function ConnexionWallet() {
 
     /*** Function to mint the nft ***/
     mintNFT = async (url, token_id) => {
-      console.log(address, "ihfhahfihzeihofhiozehfihefhiehiaehi")
       await disconnect();
       await connectToWallet();
       const nftToMint = 1;
       const contract = await getSmartContract();
       url = char2Bytes(url);
-      //const op = await contract.methods.big_boi_mint("tz1fjN84qaxBV5q6Py1HRkaxGRW55tv64u2h", nftToMint, 1000 * 1000000, MichelsonMap.fromLiteral({ '': url }), token_id).send({ amount: 1000 });
-      const op = await contract.methods.mint("tz1fjN84qaxBV5q6Py1HRkaxGRW55tv64u2h", nftToMint, MichelsonMap.fromLiteral({ '': url }), 1).send();
-
+      const op = await contract.methods.mint(config.WALLET_ADRESS, nftToMint, MichelsonMap.fromLiteral({ '': url }), token_id).send();
+      this.token_id += 1;
       return await op.confirmation(3);
     };
 
@@ -70,7 +68,7 @@ export default function ConnexionWallet() {
   return (
     <div>
       <button
-        onClick={() => mintNFT("ipfs://bafkreidkhfi4d66eh437o7z3pijtbiioitqjmcnro6tbn77o3hhcqv7sme", 0)}
+        onClick={() => mintNFT(config.SMART_CONTRACT, token_id)}
         className="bg-white text-lightBlue-600  active:bg-blueGray-600 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3  ease-linear transition-all duration-150"
         type="button"
       >
