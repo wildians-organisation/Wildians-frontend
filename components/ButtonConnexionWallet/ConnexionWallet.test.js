0e24d415b6ede8474ca9640a043e684389db429a
Hugo Bringout <hugo.bringout@epita.fr>
2022-10-31 14:50:38 +0100

    addes 2 tests for tezos

--- components/ButtonConnexionWallet/ConnexionWallet.test.js
+++ components/ButtonConnexionWallet/ConnexionWallet.test.js
@@ -1,5 +1,21 @@
 //const ConnexionWallet = require('./ConnexionWallet.js');
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(3).toBe(3);
+const { TezosToolkit } = require("@taquito/taquito");
+const { NetworkType } = require("@airgap/beacon-sdk");
+const { BeaconWallet } = require("@taquito/beacon-wallet");
+
+const network = { type: NetworkType.GHOSTNET };
+const wallet = new BeaconWallet({ name: "Test" });
+const Tezos = new TezosToolkit('https://ghostnet.smartpy.io');
+
+/* TESTS TO CHECK THAT API IS ALIVE AND SMART CONTRACT ACCESSIBLE */
+
+test('Check API connectivity', async () => {
+    const cli = wallet.client;
+    expect(cli).not.toBe(null);
+});
+
+test('Check Smart Contract', async () => {
+    const contract = await Tezos.wallet.at('KT1LSrvFWU9DKwSP1zqyDSEUw4b6qLWuaii8');
+    expect(contract).not.toBe(null);
 });
