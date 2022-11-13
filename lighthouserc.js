6745d1ac039004e9034b49bbc34cf61fdc035019
alexandreyap <alexandre.yap@epita.fr>
2022-11-13 18:37:35 +0100

    revert minScore to 70 for performance, 80 for the rest

--- lighthouserc.js
+++ lighthouserc.js
@@ -12,10 +12,10 @@ module.exports = {
       },
       assert: {
         assertions: {
-          'categories:performance': ['warn', {minScore: 1}],
-          'categories:accessibility': ['error', {minScore: 1}],
-          'categories:best-practices': ['error', {minScore: 1}],
-          'categories:seo': ['error', {minScore: 1}],
+          'categories:performance': ['warn', {minScore: 0.7}],
+          'categories:accessibility': ['error', {minScore: 0.8}],
+          'categories:best-practices': ['error', {minScore: 0.8}],
+          'categories:seo': ['error', {minScore: 0.8}],
         }
       },
     },
