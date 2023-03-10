24d29a21042da71c8eec9938c13a872a7c4b4b5f
Amina SAID ALI <amina.said-ali@epita.fr>
2023-03-10 05:42:12 +0100

    [REFACTOR] Coding style

--- components/AdminDashBoard/Layout.js
+++ components/AdminDashBoard/Layout.js
@@ -3,7 +3,7 @@ import SideBar from "./SideBar";
 import TopBar from "./TopBar";
 import { Transition } from "@headlessui/react";
 
-export default function Layout ({ children }) {
+export default function Layout({ children }) {
     /*handle mobile or laptop layout
     By default it is laptop*/
 
@@ -11,50 +11,49 @@ export default function Layout ({ children }) {
     const [isMobile, setIsMobile] = useState(false);
 
     function handleResize() {
-         //640 pixel is the width of mobile in tailwind css
+        //640 pixel is the width of mobile in tailwind css
         if (innerWidth <= 640) {
-          setShowNav(false);
-          setIsMobile(true);
+            setShowNav(false);
+            setIsMobile(true);
         } else {
-          setShowNav(true);
-          setIsMobile(false);
+            setShowNav(true);
+            setIsMobile(false);
         }
-      }
+    }
 
     useEffect(() => {
         if (typeof window != undefined) {
-          addEventListener("resize", handleResize);
+            addEventListener("resize", handleResize);
         }
         //when the component is unmounted
 
         return () => {
-          removeEventListener("resize", handleResize);
+            removeEventListener("resize", handleResize);
         };
-      }, []);
+    }, []);
 
     return (
         <>
-          <TopBar showNav={showNav} setShowNav={setShowNav} />
-          <Transition
-            as={Fragment}
-            show={showNav}
-            enter="transform transition duration-[400ms]"
-            enterFrom="-translate-x-full"
-            enterTo="translate-x-0"
-            leave="transform duration-[400ms] transition ease-in-out"
-            leaveFrom="translate-x-0"
-            leaveTo="-translate-x-full"
-          >
-            <SideBar showNav={showNav} />
-          </Transition>
-          <main
-            className={`pt-16 transition-all duration-[400ms] ${
-              showNav && !isMobile ? "pl-56" : ""
-            }`}
-          >
-            <div className="px-4 md:px-16">{children}</div>
-          </main>
+            <TopBar showNav={showNav} setShowNav={setShowNav} />
+            <Transition
+                as={Fragment}
+                show={showNav}
+                enter="transform transition duration-[400ms]"
+                enterFrom="-translate-x-full"
+                enterTo="translate-x-0"
+                leave="transform duration-[400ms] transition ease-in-out"
+                leaveFrom="translate-x-0"
+                leaveTo="-translate-x-full"
+            >
+                <SideBar showNav={showNav} />
+            </Transition>
+            <main
+                className={`pt-16 transition-all duration-[400ms] ${
+                    showNav && !isMobile ? "pl-56" : ""
+                }`}
+            >
+                <div className="px-4 md:px-16">{children}</div>
+            </main>
         </>
-      );
-
+    );
 }
