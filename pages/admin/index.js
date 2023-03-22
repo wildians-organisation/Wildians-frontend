6292d2b2d130242023d086ca5535b1f13425796b
Amina SAID ALI <amina.said-ali@epita.fr>
2023-03-22 14:15:04 +0100

    [FIX] Coding Style

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -148,14 +148,13 @@ export default function Admin() {
         };
     });
 
-
     return (
         <>
             <Layout>
                 <p className="text-gray-700 text-3xl mb-16 font-bold">
                     Wallet info
                 </p>
-                <DashboardStatsGrid/>
+                <DashboardStatsGrid />
                 <TransactionChart />
                 <RecentOrders recentTransacData={data} />
             </Layout>
