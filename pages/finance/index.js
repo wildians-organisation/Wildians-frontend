64fae3d92ae3269fea2d2479e3f2531800dce19c
Franck Hu <byfranckhu@gmail.com>
2023-03-22 13:30:06 +0100

    [ADD]

--- pages/finance/index.js
+++ pages/finance/index.js
@@ -7,13 +7,15 @@ import Layout from "../../components/AdminDashBoard/Layout";
 import { Component } from "react/cjs/react.production.min.js";
 
 const firebaseConfig = {
-    apiKey: `${config.GCPAPIKEY}`,
-    authDomain: `${config.GCPAUTHDOMAIN}`,
-    databaseURL: `${config.GCPDATABASEURL}`,
-    projectId: `${config.GCPPROJECTID}`,
-    storageBucket: `${config.GCPSTORAGEBUCKET}`,
-    messagingSenderId: `${config.GCPMESSAGINGSENDERID}`,
-    appId: `${config.GCPAPPID}`
+    apiKey: "AIzaSyBSHruPb10RXmD9pV71vsoXFhltiUztJME",
+    authDomain: "aerobic-guide-364710.firebaseapp.com",
+    databaseURL:
+        "https://aerobic-guide-364710-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "aerobic-guide-364710",
+    storageBucket: "aerobic-guide-364710.appspot.com",
+    messagingSenderId: "56985705002",
+    appId: "1:56985705002:web:add456f54103a40817c153",
+    measurementId: "G-22ZZYQGFRS"
 };
 
 export default function Admin() {
