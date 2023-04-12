39f8ca5aed1134ddcc3c1e00b5123e6bb7da0ee1
Amina SAID ALI <amina.said-ali@epita.fr>
2023-04-12 21:22:27 +0200

    [FIX] separate fetch Data into two functions + verification on smartcontract

--- pages/admin/finance/index.js
+++ pages/admin/finance/index.js
@@ -24,8 +24,9 @@ const firebaseConfig = {
 
 export default function Admin() {
     // Display items in a list with add button on each items
-    const [detailsDons, setdetailsDons] = React.useState([]);
-    const [dataFinance, setDataFinance] = React.useState([
+    let [userNFTs, setUserNFTs] = React.useState([]);
+    let [detailsDons, setDetailsDons] = React.useState([]);
+    let [dataFinance, setDataFinance] = React.useState([
         { name: "ENVIRONMENT", value: 0 },
         { name: "SOCIETY", value: 0 },
         { name: "ECONOMY", value: 0 }
@@ -36,17 +37,110 @@ export default function Admin() {
             `https://api.ghostnet.tzkt.io/v1/contracts/${config.CONTRACT_ADDRESS}/storage/history?limit=1000`
         );
         var wallets = [];
-        response.data.forEach((element) => {
+        console.log("hello");
+        console.log(dataFinance);
+        let environment = 0;
+        let society = 0;
+        let economy = 0;
+
+        // Create an array of promises for fetchData calls
+        const fetchDataPromises = response.data.map(async (element) => {
             if (element.operation.type != "origination") {
                 let data_value = element.operation.parameter.value;
 
-                if (wallets.includes(data_value.address) == false) {
+                if (!wallets.includes(data_value.address)) {
                     wallets.push(data_value.address);
-                    fetchData(data_value.address);
+                    let result = await fetchData(data_value.address);
+                    environment += result.ENVIRONMENT;
+                    society += result.SOCIETY;
+                    economy += result.ECONOMY;
                 }
             }
         });
+
+        // Wait for all promises to resolve
+        await Promise.all(fetchDataPromises);
+
+        setDataFinance((prevState) => {
+            const newData = [...prevState];
+            newData[0].value = environment;
+            newData[1].value = society;
+            newData[2].value = economy;
+            return newData;
+        });
+        console.log(dataFinance);
+
         setUserNFTs(wallets);
+    };
+
+    // Define a function to process data
+    const processResponseData = (responseData) => {
+        let ENVIRONMENT = 0;
+        let SOCIETY = 0;
+        let ECONOMY = 0;
+
+        for (let i = 0; i < responseData["data"].length; i++) {
+            if (
+                responseData["data"][i]["token"]["metadata"] == null ||
+                responseData["data"][i]["token"]["metadata"] == undefined ||
+                responseData["data"][i]["token"]["contract"]["address"] !=
+                    config.CONTRACT_ADDRESS
+            )
+                continue;
+            else {
+                let dateT = responseData["data"][i]["firstTime"];
+                let userAddressToFetch =
+                    responseData["data"][i]["account"]["address"];
+                if (
+                    responseData["data"][i]["token"]["metadata"]["name"] ==
+                    WildiansTypes.BICHE
+                ) {
+                    ENVIRONMENT += 1;
+
+                    setDetailsDons((prevDetails) => [
+                        ...prevDetails,
+                        {
+                            address: userAddressToFetch,
+                            ong: "WWF",
+                            date: new Date(dateT).toLocaleString()
+                        }
+                    ]);
+                } else if (
+                    responseData["data"][i]["token"]["metadata"]["name"] ==
+                    WildiansTypes.WOLF
+                ) {
+                    SOCIETY += 1;
+
+                    setDetailsDons((prevDetails) => [
+                        ...prevDetails,
+                        {
+                            address: userAddressToFetch,
+                            ong: "Greenpeace",
+                            date: new Date(dateT).toLocaleString()
+                        }
+                    ]);
+                } else if (
+                    responseData["data"][i]["token"]["metadata"]["name"] ==
+                    WildiansTypes.BULL
+                ) {
+                    ECONOMY += 1;
+
+                    setDetailsDons((prevDetails) => [
+                        ...prevDetails,
+                        {
+                            address: userAddressToFetch,
+                            ong: "Unicef",
+                            date: new Date(dateT).toLocaleString()
+                        }
+                    ]);
+                }
+            }
+        }
+        return {
+            ENVIRONMENT: ENVIRONMENT,
+            SOCIETY: SOCIETY,
+            ECONOMY: ECONOMY
+        };
     };
 
     const fetchData = async (userAddressToFetch) => {
@@ -54,74 +148,8 @@ export default function Admin() {
             const response = await axios.get(
                 `https://api.ghostnet.tzkt.io/v1/tokens/balances?account=${userAddressToFetch}`
             );
-            console.log("debut");
-
-            for (let i = 0; i < response["data"].length; i++) {
-                let dateT = response["data"][i]["firstTime"];
-                //console.log(response["data"][i]["token"]["contract"]["address"]);
-                //console.log(response["data"][i]["token"]["contract"]["address"] == config.CONTRACT_ADDRESS);
-                if (
-                    response["data"][i]["token"]["metadata"] == null ||
-                    response["data"][i]["token"]["metadata"] == undefined ||
-                    response["data"][i]["token"]["contract"]["address"] !=
-                        config.CONTRACT_ADDRESS
-                )
-                    continue;
-                else {
-                    console.log("data th -->  " + i);
-                    console.log(
-                        response["data"][i]["token"]["contract"]["address"] ==
-                            config.CONTRACT_ADDRESS
-                    );
-                    if (
-                        response["data"][i]["token"]["metadata"]["name"] ==
-                        WildiansTypes.BICHE
-                    ) {
-                        setDataFinance((prevState) => {
-                            const newData = [...prevState];
-                            newData[0].value = newData[0].value + 1;
-                            return newData;
-                        });
-
-                        detailsDons.push({
-                            address: userAddressToFetch,
-                            ONG: "WWF",
-                            date: new Date(dateT).toLocaleString()
-                        });
-                    } else if (
-                        response["data"][i]["token"]["metadata"]["name"] ==
-                        WildiansTypes.WOLF
-                    ) {
-                        setDataFinance((prevState) => {
-                            const newData = [...prevState];
-                            newData[1].value = newData[1].value + 1;
-                            return newData;
-                        });
-                        detailsDons.push({
-                            address: userAddressToFetch,
-                            ONG: "Greenpeace",
-                            date: new Date(dateT).toLocaleString()
-                        });
-                    } else if (
-                        response["data"][i]["token"]["metadata"]["name"] ==
-                        WildiansTypes.BULL
-                    ) {
-                        setDataFinance((prevState) => {
-                            const newData = [...prevState];
-                            newData[2].value = newData[2].value + 1;
-                            return newData;
-                        });
-                        detailsDons.push({
-                            address: userAddressToFetch,
-                            ONG: "Unicef",
-                            date: new Date(dateT).toLocaleString()
-                        });
-                    }
-                }
-            }
-            console.log(detailsDons);
-            console.log("hello");
-            console.log(dataFinance);
+            let result = processResponseData(response);
+            return result;
         } catch (e) {
             console.error(e);
         }
@@ -129,6 +157,7 @@ export default function Admin() {
 
     React.useEffect(() => {
         getContractInformations();
+        console.log(dataFinance);
     }, []);
 
     return (
