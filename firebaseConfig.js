ad9e213893ce6e5a4336bf7c74aea462125df21b
Thomas Saison <tsaison9@gmail.com>
2023-04-24 18:50:21 +0200

    Fix analytics

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -15,6 +15,8 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
 const firestore = getFirestore();
 
-module.exports = { firestore };
+module.exports = { firestore, analytics };
