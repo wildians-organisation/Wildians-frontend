08477aa246cc5e4b7cc03634c49612e43c7af9ba
Johnny CHEN <johnnychen@hotmail.fr>
2023-05-06 09:14:49 +0200

    [Fix] Grammar error fixed

--- components/Wildian/Wildians.js
+++ components/Wildian/Wildians.js
@@ -7,8 +7,6 @@ import { BeaconWallet } from "@taquito/beacon-wallet";
 import { NetworkType } from "@airgap/beacon-sdk";
 import axios from "axios";
 import Modal from "./Modal.js";
-import ReactModal from "react-modal";
-
 
 const nftToMint = 1;
 
@@ -129,7 +127,7 @@ function Wildians(Wildians) {
             <button
                 onClick={openModal}
                 className="mintNFT text-gray-900 group flex rounded-full items-center px-2 py-2 md:h-min md:text-sm md:text-greenkaki md:bg-greeny md:hover:bg-greenkaki md:hover:text-greeny  md:text-xs md:font-bold md:uppercase md:px-4 md:py-2 md:rounded-full md:shadow md:hover:shadow-lg md:outline-none md:focus:outline-none md:mr-1 md:mb-0 md:ml-3  md:ease-linear md:transition-all md:duration-150 md:whitespace-nowrap "
-                type="button">Select a ONG
+                type="button">Select an ONG
             </button>
             <Modal isOpen={showModal} onClose={closeModal} onMint={mintNFT} Wildians={Wildians}> </Modal>
         </div>
