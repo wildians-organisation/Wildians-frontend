32ba97be91bbe6578161ea0347f87c8c01b22914
Franck Hu <byfranckhu@outlook.com>
2023-10-23 20:23:45 +0200

    [ADD] font for usage part

--- components/LandingPage/UsageItem.tsx
+++ components/LandingPage/UsageItem.tsx
@@ -1,8 +1,8 @@
 export function UsageItem({ title, content }) {
     return (
-        <div className="w-[600px]">
-            <span className="text-white text-xl font-extrabold">{title}</span>
-            <p className="text-white text-base">{content}</p>
+        <div className="w-[600px] ">
+            <span className="text-white subtitle-typo">{title}</span>
+            <p className="text-white body-typo">{content}</p>
         </div>
     );
 }
