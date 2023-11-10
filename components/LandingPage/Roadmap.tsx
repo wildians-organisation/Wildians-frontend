627a84a4a54801bf23b942d4f5f5c41434a76cb3
Johnny CHEN <johnnychen@hotmail.fr>
2023-11-10 11:35:35 +0100

    [ADD] Roadmap without years

---
+++ components/LandingPage/Roadmap.tsx
@@ -0,0 +1,44 @@
+import { Disclosure } from "@headlessui/react";
+import React from "react";
+export default function RoadMapVitrine() {
+    return (
+<div className="timeline">
+    <h1 className=" text-2xl text-white text-center font-bold mb-16">
+        Notre feuille de route
+    </h1>
+    <ol>
+        <li>Presentation du concept Wildians</li>
+        <li><b>Crowdfunding: </b> <br />Première rencontre avec ton Wildian</li>
+        <li><b>Alpha de l'application: </b> <br />Ouverture  à une sélection 
+d’ambassadeurs</li>
+        <li><b>Sortie de l’application: </b>
+        <br />
+        <ul >
+             Premières fonctionnalités
+            <br />
+            Premiers parcours
+        </ul>
+</li>
+    </ol>
+</div>
+
+
+//         <div className="center">
+//             <h1 className=" text-2xl text-white text-center font-bold mb-16">
+//                 Notre feuille de route
+//             </h1>
+//             <div className="container mx-auto mt-10">
+//     <div className="relative">
+//       <div className="absolute top-1/2 left-0 transform -translate-y-1/2 w-full h-1 bg-gray-300"></div>
+//       <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-6 h-6 bg-blue-500 rounded-full"></div>
+
+//       <div className="flex justify-between">
+//         <div className="w-6 h-6 bg-green-500 rounded-full"></div>
+//         <div className="w-6 h-6 bg-yellow-500 rounded-full"></div>
+//         <div className="w-6 h-6 bg-red-500 rounded-full"></div>
+//       </div>
+//     </div>
+//   </div>
+//         </div>
+    );
+}
