def1a2289b00fdaab3bf7cf1a073900c7757e562
Alec LIM <alec.lim@epita.fr>
2023-11-10 12:44:28 +0300

    [FIX] bug

--- components/LandingPage/Support.tsx
+++ components/LandingPage/Support.tsx
@@ -112,8 +112,10 @@ export default function Support() {
                                             <strong>indispensable</strong>.
                                             Cette app sera conçue pour t'
                                             <strong>accompagner</strong>, pas à
-                                            pas, dans ta transition vers un <strong className="text-greeny">mode
-                                            de vie plus durable.</strong>
+                                            pas, dans ta transition vers un{" "}
+                                            <strong className="text-greeny">
+                                                mode de vie plus durable.
+                                            </strong>
                                         </p>
                                         <p>
                                             Elle t'aidera à comprendre les
