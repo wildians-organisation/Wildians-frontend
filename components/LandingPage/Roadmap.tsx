721843f8689a4730baaa7422cb90592df4711c2e
Johnny CHEN <johnnychen@hotmail.fr>
2023-11-22 23:32:14 +0100

    [Fix] Roadmap + Responsive

--- components/LandingPage/Roadmap.tsx
+++ components/LandingPage/Roadmap.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 export default function RoadMapVitrine() {
     return (
-
         <div>
-            <h1 className="text-center text-2xl text-white font-bold mb-16">
+            <h1 className=" text-l text-white font-bold mb-16 text-left-20">
                 Notre feuille de route
             </h1>
             <ul className="timeline">
@@ -12,15 +11,26 @@ export default function RoadMapVitrine() {
                         <span className="Date">&nbsp;</span>
                     </div>
                     <div className="year2023">
-                        <br />&nbsp;<br />&nbsp;<br />&nbsp;
+                        <br />
+                        &nbsp;
+                        <br />
+                        &nbsp;
+                        <br />
+                        &nbsp;
                     </div>
                 </li>
                 <li className="li complete">
                     <div className="timestamp">
-                        <span className="Date"><b>Octobre</b></span>
+                        <span className="Date">
+                            <b>Octobre</b>
+                        </span>
                     </div>
                     <div className="status">
-                        <h4>Presentation du concept Wildians<br /><p>&nbsp;</p> <br /></h4>
+                        <h4>
+                            Presentation du concept Wildians
+                            <br />
+                            <p>&nbsp;</p> <br />
+                        </h4>
                     </div>
                 </li>
                 <li className="li complete">
@@ -28,17 +38,25 @@ export default function RoadMapVitrine() {
                         <span className="Date">&nbsp;</span>
                     </div>
                     <div className="year2024">
-                        <br />&nbsp;<br />&nbsp;<br />&nbsp;
+                        <br />
+                        &nbsp;
+                        <br />
+                        &nbsp;
+                        <br />
+                        &nbsp;
                     </div>
                 </li>
                 <li className="li complete">
                     <div className="timestamp">
-                        <span className="Date"><b> Q1 </b></span>
+                        <span className="Date">
+                            <b> Q1 </b>
+                        </span>
                     </div>
                     <div className="status">
                         <h4>
-                            <b>Crowdfunding: </b> <br /> 
-                            Première rencontre avec ton Wildian <br /> <p>&nbsp;</p>
+                            <b>Crowdfunding: </b> <br />
+                            Première rencontre avec ton Wildian <br />{" "}
+                            <p>&nbsp;</p>
                         </h4>
                     </div>
                 </li>
@@ -47,8 +65,11 @@ export default function RoadMapVitrine() {
                         <span className="Date">&nbsp;</span>
                     </div>
                     <div className="status">
-                        <h4><b>Alpha de l'application: </b> <br />
-                    Ouverture à une sélection d’ambassadeurs <br />&nbsp; </h4>
+                        <h4>
+                            <b>Alpha de l'application: </b> <br />
+                            Ouverture à une sélection d’ambassadeurs <br />
+                            &nbsp;{" "}
+                        </h4>
                     </div>
                 </li>
                 <li className="li complete">
@@ -56,11 +77,13 @@ export default function RoadMapVitrine() {
                         <span className="Date">&nbsp;</span>
                     </div>
                     <div className="status">
-                        <h4>   <b>Sortie de l’application: </b>
-                    <br />
-                        • Premières fonctionnalités
-                        <br />
-                        • Premiers parcours</h4>
+                        <h4>
+                            {" "}
+                            <b>Sortie de l’application: </b>
+                            <br />
+                            • Premières fonctionnalités
+                            <br />• Premiers parcours
+                        </h4>
                     </div>
                 </li>
             </ul>
