3711a9e49c3d1e3205509f5902132b71fa650576
Alec LIM <alec.lim@epita.fr>
2023-12-22 02:02:07 +0300

    [FIX] deploy site de vente dev

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ export function scrollToSection(sectionId) {
 
 export default function Landing() {
     return (
-        <div style={{ backgroundColor: "#223734" }}>
+        /*<div style={{ backgroundColor: "#223734" }}>
             <TopPartVitrine />
             <Concept />
             <Separator />
@@ -42,8 +42,8 @@ export default function Landing() {
             <RoadMapVitrine />
             <Team />
             <BottomPartVitrine />
-        </div>
-        /*<div style={{ backgroundColor: "#223734" }}>
+        </div>*/
+        <div style={{ backgroundColor: "#223734" }}>
             <TopPart />
             <Concept />
             <Usage />
@@ -51,6 +51,6 @@ export default function Landing() {
             <Support />
             <Team />
             <BottomPartVitrine />
-        </div>*/
+        </div>
     );
 }
